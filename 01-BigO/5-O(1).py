94267e4303510a1536f5f5cb71032bf1cc62161f
BarathSrinivasan2002 <sam.b.srinivasan2002@gmail.com>
2025-04-23 10:38:42 -0400

    Learning BigO notation

---
+++ 01-BigO/5-O(1).py
@@ -0,0 +1,4 @@
+def add(n):
+    n+n
+
+# o(1) is a constant time regardless of how many operations.
---
+++ 01-BigO/Different+Terms.py
@@ -0,0 +1,10 @@
+def print_items(a,b):
+    for i in range(a):
+        print(i)
+
+    for j in range(b):
+        print(j)
+
+print_items(1, 10)
+
+# time complexity = O(a+b) if it was nessted for loop then its O(a*b)
